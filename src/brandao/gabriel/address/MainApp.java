c5cf1af16e825f1061d4904c2eeb9f6ce93b16be
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2019-04-27 14:52:06 -0300

    Create showPersonEditDialog()

--- src/brandao/gabriel/address/MainApp.java
+++ src/brandao/gabriel/address/MainApp.java
@@ -7,6 +7,7 @@ package brandao.gabriel.address;
 
 
 import brandao.gabriel.address.model.Person;
+import brandao.gabriel.address.view.PersonEditDialogController;
 import brandao.gabriel.address.view.PersonOverViewController;
 import java.io.IOException;
 import javafx.application.Application;
@@ -16,6 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -85,23 +87,59 @@ public class MainApp extends Application {
      * Mostra o person overview dentro do root layout.
      */
     public void showPersonOverview() {
-    try {
-        // Carrega a person overview.
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(MainApp.class.getResource("view/PersonOverView.fxml"));
-        AnchorPane personOverview = (AnchorPane) loader.load();
+        try {
+            // Carrega a person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverView.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
 
-        // Define a person overview no centro do root layout.
-        rootLayout.setCenter(personOverview);
+            // Define a person overview no centro do root layout.
+            rootLayout.setCenter(personOverview);
 
-        // Dá ao controlador acesso à the main app.
-        PersonOverViewController controller = loader.getController();
-        controller.setMainApp(this);
+            // Dá ao controlador acesso à the main app.
+            PersonOverViewController controller = loader.getController();
+            controller.setMainApp(this);
 
-    } catch (IOException e) {
-        e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+    /**
+    * Abre uma janela para editar detalhes para a pessoa especificada. Se o usuário clicar
+    * OK, as mudanças são salvasno objeto pessoa fornecido e retorna true.
+    * 
+    * @param person O objeto pessoa a ser editado
+    * @return true Se o usuário clicou OK,  caso contrário false.
+    */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Carrega o arquivo fxml e cria um novo stage para a janela popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Cria o palco dialogStage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Define a pessoa no controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Mostra a janela e espera até o usuário fechar.
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
     
     /**
      * Retorna o palco principal.
