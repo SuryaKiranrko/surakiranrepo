2b4c77606df811c83296907b581360c01e2e064b
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2019-04-27 13:36:36 -0300

    Add PersonOverViewController funcionality

--- src/brandao/gabriel/address/view/PersonOverViewController.java
+++ src/brandao/gabriel/address/view/PersonOverViewController.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package brandao.gabriel.address.view;
 
-/**
- *
- * @author gabri_000
- */
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import brandao.gabriel.address.MainApp;
+import brandao.gabriel.address.model.Person;
+
 public class PersonOverViewController {
-    
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    // Reference to the main application.
+    private MainApp mainApp;
+
+    /**
+     * O construtor.
+     * O construtor é chamado antes do método inicialize().
+     */
+    public PersonOverViewController() {
+    }
+
+    /**
+     * Inicializa a classe controller. Este método é chamado automaticamente
+     *  após o arquivo fxml ter sido carregado.
+     */
+    @FXML
+    private void initialize() {
+        // Inicializa a tablea de pessoa com duas colunas.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    /**
+     * É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Adiciona os dados da observable list na tabela
+        personTable.setItems(mainApp.getPersonData());
+    }
 }
