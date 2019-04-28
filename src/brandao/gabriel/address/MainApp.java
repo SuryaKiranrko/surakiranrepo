b9f7015bded79b77236624f81b8f0356ea6f81a3
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2019-04-28 00:48:43 -0300

    Add getPersonFilePath() and public void setPersonFilePath(File file)

--- src/brandao/gabriel/address/MainApp.java
+++ src/brandao/gabriel/address/MainApp.java
@@ -9,7 +9,9 @@ package brandao.gabriel.address;
 import brandao.gabriel.address.model.Person;
 import brandao.gabriel.address.view.PersonEditDialogController;
 import brandao.gabriel.address.view.PersonOverViewController;
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -155,5 +157,43 @@ public class MainApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    /**
+    * Retorna o arquivo de preferências da pessoa, o último arquivo que foi aberto.
+    * As preferências são lidas do registro específico do SO (Sistema Operacional). 
+    * Se tais prefêrencias não puderem  ser encontradas, ele retorna null.
+    * 
+    * @return
+    */
+    public File getPersonFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+    * Define o caminho do arquivo do arquivo carregado atual. O caminho é persistido no
+    * registro específico do SO (Sistema Operacional).
+    * 
+    * @param file O arquivo ou null para remover o caminho
+    */
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
     }
 }
