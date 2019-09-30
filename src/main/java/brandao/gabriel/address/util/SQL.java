d61d694960bca39f60971088a1e0d6b5249fe835
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-09-30 08:59:30 -0300

    Use properties to get db parameters

--- src/main/java/brandao/gabriel/address/util/SQL.java
+++ src/main/java/brandao/gabriel/address/util/SQL.java
@@ -1,25 +1,31 @@
 package brandao.gabriel.address.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public final class SQL {
     private static final Connection SQL_CONNECTION;
-    private static final String URL;
-    private static final String USER;
-    private static final String PASSWORD;
 
     static {
-        URL = "jdbc:postgresql://localhost:5432/";
-        USER = "postgres";
-        PASSWORD = "";
         SQL_CONNECTION = connect();
     }
 
     public SQL(){};
 
     private static Connection connect() {
+        String url = "";
+        String user = "";
+        String password = "";
+        try {
+            Properties dbProps = getProp();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -27,14 +33,19 @@ public final class SQL {
         }
         Connection conn = null;
         try {
-            System.out.println(URL);
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conectado no BD.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return conn;
+    }
+
+    public static Properties getProp() throws IOException {
+        Properties props = new Properties();
+        props.load(SQL.class.getResourceAsStream("/dbproperties.properties"));
+        return props;
     }
 
     public static Connection getConnection() {
